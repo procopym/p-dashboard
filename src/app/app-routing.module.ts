c507c9f594f6e5eadafaff91b79e0f1ae0c66f26
Mihai Procopi <procopy.mihai@gmail.com>
2020-07-11 02:03:18 +0300

    Diary UI structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, NoPreloading} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {ErrorPageComponent} from "./pages/error-page/error-page.component";
+import {AppComponent} from "./app.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      { path: '', component: HomeComponent}
+    ]
+  },
+  { path: 'error', component: ErrorPageComponent },
+  { path: 'diary', loadChildren: () => import('./modules/diary/diary.module').then(mod => mod.DiaryModule) },
+  { path: '**', redirectTo: '/error' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: NoPreloading})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {ErrorPageComponent} from './pages/error-page/error-page.component';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
